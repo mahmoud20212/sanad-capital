fee1d0d39f213b12f05dc4076ca0b8ed19cadf43
mahmoud20212 <occee123@gmail.com>
2023-06-18 21:32:36 +0300

    new update

--- asset/js/base.js
+++ asset/js/base.js
@@ -2,7 +2,8 @@ const wrapper = document.querySelector('.wrapper');
 const sidebar = document.querySelector('.sidebar');
 const content = document.querySelector('.content');
 const menu = document.querySelector('#show-menu');
-const closeSidebar = document.querySelector('#close-menu')
+const closeSidebar = document.querySelector('#close-menu');
+const allLinkTag = document.querySelectorAll('.link-tag');
 
 // sidebar toggle menu
 menu.addEventListener('click', () => {
@@ -15,4 +16,25 @@ closeSidebar.addEventListener('click', () => {
     sidebar.classList.remove('active');
     content.classList.remove('active');
     wrapper.classList.remove('active');
+});
+
+// SVG change color
+allLinkTag.forEach(link =>{
+    // check active if in link or not
+    if (link.classList.contains('active')) return;
+
+    // event for link
+    link.addEventListener('mouseover', () => {
+        let path = link.querySelectorAll('svg path');
+        path.forEach(itemPath => {
+            itemPath.setAttribute('stroke', '#FAFAFA');
+        });
+    });
+
+    link.addEventListener('mouseleave', () => {
+        let path = link.querySelectorAll('svg path');
+        path.forEach(itemPath => {
+            itemPath.setAttribute('stroke', '#718096');
+        });
+    });
 });
